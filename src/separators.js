3433d0404c263789e4930aee511775cb400ed07f
jydawn <1198621249@qq.com>
2016-06-14 15:03:30 +0800

    提交

---
+++ src/separators.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function thousands_separators(num) {
+    num += '';
+    var x = num.split('.');
+    var x1 = x[0];
+    var x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+}
+
+module.exports = thousands_separators;
